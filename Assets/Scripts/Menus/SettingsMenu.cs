3ed34ed69a4bda135ad00084a4a384d3dae8d93d
chase cameron <chase cameron>
2018-01-08 15:01:34 -0800

    Added the menu system and changed the main menu to fit the game better.

---
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.UI;
+
+public class SettingsMenu: SimpleMenu<SettingsMenu> {
+	public void OnControlsPressed() {
+		//Debug.Log("Controls button was pressed.");
+		//ControlsMenu.Show();
+	}
+	
+	public void OnGraphicsPressed() {
+		//Debug.Log("Graphics button was pressed.");
+		//GraphicsMenu.Show();
+	}
+	
+	public void OnAudioPressed() {
+		//Debug.Log("Audio button was pressed.");
+		//AudioMenu.Show();
+	}
+	
+	//public Slider Slider;
+}
