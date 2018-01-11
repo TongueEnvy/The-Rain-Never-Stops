b92030312a416366d7b6439a81ec88d60396b54b
chase cameron <chase cameron>
2018-01-10 16:10:02 -0800

    Created a RoomManager to create an endless room cycle.

---
+++ Assets/Scripts/RoomManager/RoomManager.cs
@@ -0,0 +1,63 @@
+﻿/**************************
+** RoomManager.cs
+** Chase McWhirt
+** January 10th, 2018
+***************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager: MonoBehaviour {
+	public const float roomSize	=	12f;
+	public float riseSpeed	=		0.03f;
+	public List<GameObject> potentialRooms;
+	public GameObject startRoom;
+	public GameObject player;
+	public int maxRoomBuffer =	5;
+	//public static RoomManager Instance { get; set; }
+	
+	private GameObject selectedRoom;
+	private int rng;
+	private Queue<GameObject> loadedRooms;
+	
+    void Start() {
+		Random.InitState(System.Environment.TickCount);
+		loadedRooms = new Queue<GameObject>();
+        loadedRooms.Enqueue(startRoom);
+	}
+	
+	void Update() {
+		//Flood increase rate 0 to 100
+
+		if(player.transform.position.y >= -2f) {
+			player.transform.position =
+				new Vector2(player.transform.position.x,
+							player.transform.position.y - roomSize);
+			
+			foreach(GameObject room in loadedRooms) {
+				room.transform.position =
+					new Vector2(0, room.transform.position.y - roomSize);
+			}
+			
+			rng = Random.Range(8, potentialRooms.Count);
+			//Instantiate(prefab, transform);
+			selectedRoom = Instantiate<GameObject>(potentialRooms[rng], null);
+			selectedRoom.transform.position = new Vector2(0, 0);
+			loadedRooms.Enqueue(selectedRoom);
+
+			if(loadedRooms.Count == (maxRoomBuffer + 3)) {
+				GameObject room = loadedRooms.Dequeue();
+				Destroy(room.gameObject);
+				//floodPercentage = 0f;
+				//floodPosition = 0 (bottom of queue);
+			}
+		}
+		
+		//This conditional is where the flood has engulfed a floor.
+		else if(true == false) {
+			//loadedRooms.Peek().destroy(self);
+			loadedRooms.Dequeue();
+			//floodPercentage = 0f;
+		}
+	}
+}
--- Assets/Scripts/scr_theFlood.cs
+++ Assets/Scripts/scr_theFlood.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class scr_theFlood : MonoBehaviour {
-
+public class scr_theFlood: MonoBehaviour {
     public float riseSpeed;
     public GameObject player;
     public float playerPosMod;
@@ -15,8 +14,11 @@ public class scr_theFlood : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        transform.position = new Vector2 (transform.position.x, transform.position.y + (riseSpeed + (player.transform.position.y * playerPosMod)));
-
+        transform.position = new Vector2
+			(transform.position.x,
+			transform.position.y + (riseSpeed +
+			//Very soft catch up mechanic.
+				(player.transform.position.y * playerPosMod))
+			);
 	}
 }
