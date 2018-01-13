ece1be3a530fd1abd7fa4e0b749a02f9a8ab8c5c
TongueEnvy <mr.dfelkins@gmail.com>
2018-01-12 20:48:47 -0800

    -Added brief intro -camera pans to center of tower when door is entered -glowing aesthetic for the lettering ok?

--- Assets/Scripts/scr_theFlood.cs
+++ Assets/Scripts/scr_theFlood.cs
@@ -7,14 +7,18 @@ public class scr_theFlood: MonoBehaviour {
     public GameObject player;
     public float playerPosMod;
 
-	// Update is called once per frame
-	void Update () {
-        transform.position =
-			new Vector2(transform.position.x,
-				transform.position.y + (riseSpeed +
-				//Very soft catch up mechanic.
-					(player.transform.position.y * playerPosMod)
-				)
-			);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.GetComponent<player_move>().playerIsClimbing == true)
+        {
+            transform.position =
+                new Vector2(transform.position.x,
+                    transform.position.y + (riseSpeed +
+                        //Very soft catch up mechanic.
+                        (player.transform.position.y * playerPosMod)
+                    )
+                );
+        }
+    }
 }
---
+++ Assets/scr_startClimbing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_startClimbing : MonoBehaviour {
+
+    [HideInInspector] public Animator anim;
+    public AnimationClip close;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = gameObject.GetComponent<Animator>();
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.gameObject == player)
+        {
+
+            anim.enabled = true;
+            anim.Play(close.name);
+            player.GetComponent<player_move>().playerIsClimbing = true;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
