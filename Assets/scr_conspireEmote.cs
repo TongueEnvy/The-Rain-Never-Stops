d0243c0de98c37cb5c5b8c49ec59c90afe1a3b8f
TongueEnvy <mr.dfelkins@gmail.com>
2018-01-14 15:31:18 -0800

    Merge branch 'master' of https://github.com/TongueEnvy/The-Rain-Never-Stops
    # Conflicts:
    #	Assets/Scripts/Player/player_move.cs

---
+++ Assets/scr_conspireEmote.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class scr_conspireEmote : MonoBehaviour {
+    
+    //0 = neutral, 1 = laughing, 2 equals annoyed
+    public int mood;
+    public List<Sprite> emotes;
+    public GameObject face;
+    public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+        mood = 0;
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if(collision.gameObject == player)
+        {
+
+            mood = 2;
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.gameObject == player)
+        {
+
+            mood = 0;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        face.GetComponent<SpriteRenderer>().sprite = emotes[mood];
+
+    }
+}
